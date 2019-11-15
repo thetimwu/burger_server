c806ce9f9b10c64622bd0ab41f214fe979e4bce9
tim wu <timwuuk@gmail.com>
2019-11-15 21:19:29 +0000

    fix bugs for plain burger

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import Aux from "../../../hoc/Aux";
+import Aux from "../../../hoc/Auxios";
 import Button from "../../UI/Button/Button";
 
 class OrderSummary extends Component {
